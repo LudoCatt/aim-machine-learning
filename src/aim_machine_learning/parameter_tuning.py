3b226aed8b3622aee546290f4f4706f028b6679a
Ludovica Cattaneo <101568658+LudoCatt@users.noreply.github.com>
2022-03-31 19:20:26 +0200

    Update parameter_tuning.py

--- src/aim_machine_learning/parameter_tuning.py
+++ src/aim_machine_learning/parameter_tuning.py
@@ -1,5 +1,10 @@
 class ParametersTuner():
     def __init__(self, model_class, X, y, supported_eval_types):
-        self.model = model_class(**params)
+        self.model = model_class()
         self.X = X
         self.y = y
+        self.supported_eval_types=supported_eval_types
+
+    def tune_parameters(self, k_dict, eval_type, eval_obj, **params):
+        self.eval_type = eval_type
+        
