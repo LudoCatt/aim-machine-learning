773c2b9d215b5cabb7e05c659c24455889b58a66
Ludovica Cattaneo <101568658+LudoCatt@users.noreply.github.com>
2022-03-31 19:13:08 +0200

    prova senza DS

--- src/aim_machine_learning/parameter_tuning.py
+++ src/aim_machine_learning/parameter_tuning.py
@@ -1,5 +1,5 @@
 class ParametersTuner():
-    def __init__(self, model_class, params, X, y):
+    def __init__(self, model_class, X, y, supported_eval_types):
         self.model = model_class(**params)
         self.X = X
         self.y = y
