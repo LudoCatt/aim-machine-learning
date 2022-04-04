8fe03331f1b7d65a9a6a9ad1c080e1fb64401575
Ludovica Cattaneo <101568658+LudoCatt@users.noreply.github.com>
2022-04-04 18:52:32 +0200

    generalizzazione1

--- src/aim_machine_learning/parameter_tuning.py
+++ src/aim_machine_learning/parameter_tuning.py
@@ -1,5 +1,6 @@
 from aim_machine_learning.model_evaluator import ModelEvaluator
 import matplotlib.pyplot as plt
+import numpy as np
 
 class ParametersTuner():
     def __init__(self, model_class, X, y, supported_eval_types, **output_path):
@@ -13,6 +14,12 @@ class ParametersTuner():
         self.eval_type = eval_type
         if eval_type not in self.supported_eval_types:
             raise NameError('Metodo di valutazione non supportato')
+
+        prova=params.get('fig_name') # servirà per fare i plot eventualmente
+        if len(self.output_path)>0 and prova is not None:
+            flag=True
+        else: 
+            flag=False
 
         minimo=float('inf') # se ci sono dati viene sovrascritto
         param_res={}
@@ -29,12 +36,20 @@ class ParametersTuner():
                 if res[-1]<minimo:
                     minimo=res[-1]
                     param_res[my_k]=k
+
+            if flag:
+                plt.figure()
+                plt.plot(param_dict[my_k],res)
+                plt.xlabel(my_k)
+                plt.ylabel('Upper bound MSE')
+                plt.savefig(self.output_path['output_path']+params['fig_name'])
+
 # vediamo la miglior combinazione dei due parametri
         elif len(param_dict.keys())==2:
             my_a=list(param_dict.keys())[0]
             my_b=list(param_dict.keys())[1]
-            for a in param_dict[my_a]:
-                for b in param_dict[my_b]:
+            for b in param_dict[my_b]:
+                for a in param_dict[my_a]:
                     mod_eval = ModelEvaluator(self.model_class, {my_a:a, my_b:b}, self.X, self.y)
                     if eval_type == 'ttsplit':
                         ub_mse = mod_eval.train_test_split_eval(eval_obj, params['test_proportion'])
@@ -45,16 +60,20 @@ class ParametersTuner():
                         minimo=res[-1]
                         param_res[my_a]=a
                         param_res[my_b]=b
+            res=np.array(res).reshape((len(param_dict[my_b]), len(param_dict[my_a])))
+# considero che nel fare un disegno nel caso multivariato, b sia fissato e ci interessi la pendenza della retta
+# disegno quindi a b fissato eventulmente sovrapponendo i grafici se si è interessati a più valori di b
+            if flag:
+                plt.figure()
+                for i in range(len(param_dict[my_b])):
+                    plt.plot(param_dict[my_a], res[i,:], label='b={}'.format(param_dict[my_b][i]))
+                plt.legend(loc="upper left")
+                plt.xlabel(my_a)
+                plt.ylabel('Upper bound MSE')
+                plt.savefig(self.output_path['output_path']+params['fig_name'])
+
         else:
             raise TypeError('Si è superato il numero massimo di parametri supportato (2)')
-# considero che nel fare un disegno nel caso multivariato, b sia fissato e ci interessi la pendenza della retta
-        prova=params.get('fig_name')
-        if len(self.output_path)>0 and prova is not None:
-            plt.figure()
-            plt.plot(param_dict[list(param_dict.keys())[0]],res)
-            plt.xlabel(list(param_dict.keys())[0])
-            plt.ylabel('Upper bound MSE')
-            plt.savefig(self.output_path['output_path']+params['fig_name'])
 # per risalire al path e dare il nome ho unito i due pezzi passati
 # fig_name risulta uno degli elementi dei **params, infatti viene unito tutto
         return param_res
